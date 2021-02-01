f543746a2eead91c6667c8aac7e30a6da26e4352
NikolayPalatidi <78350114+NikolayPalatidi@users.noreply.github.com>
2021-02-01 16:45:52 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
